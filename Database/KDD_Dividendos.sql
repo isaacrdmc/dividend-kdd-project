f896e8326ea17888504f0b881a68169e0decfd4c
Isaac Ramirez <isaacrdmc@gmail.com>
2025-07-27 16:59:14 -0600

    Estructura del poryecto

---
+++ Database/KDD_Dividendos.sql
@@ -0,0 +1,34 @@
+
+-- DROP DATABASE IF EXISTS kddDividendos;
+CREATE DATABASE kddDividendos;
+USE kddDividendos;
+
+
+-- Tabla para los datos crudos obtenidos de la API:
+CREATE TABLE dividendos_raw (
+	symbol TEXT,
+    ex_dividnd_date TEXT,
+    declaration_date TEXT,
+    record_date TEXT,
+    payment_date TEXT,
+    amount REAL
+);
+
+-- Tabla para los datos transformados:
+CREATE TABLE dividendos_transformados (
+	symbol TEXT,
+    ano INTEGER,
+    dividend_avg REAL,
+    dividend_growth REAL,
+    dividend_yield REAL,
+    payout_ratio REAL
+);
+
+
+
+
+
+
+
+
+
